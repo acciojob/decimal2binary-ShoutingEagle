9a3157304af1eaac4e90df0e0f16520190afa1fb
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-07-09 20:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function threeSum(arr, target) {
+function threeSum(nums) {
 // write your code here
-  
+        let str = "";
+        let tempStr = "";
+        let temp = 0;
+        while(nums !==0){
+          temp = nums%2;
+          
+          str = str + temp;
+          nums = nums/2;
+        }
+        System.out.println(str);
+        
+        for(let i=str.length-1;i>=0;i--){
+            tempStr = tempStr + str.charAt(i);
+        }
+        return tempStr;
 }
 
 module.exports = threeSum;
